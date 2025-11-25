58389342650aeb584103ae6142272cf6a84c5234
Beatriz P. B. Alves <beatriz_bagesteiro@hotmail.com>
2025-11-25 18:43:34 -0300

    Email Route

---
+++ src/config/nodemailer.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { createTransport } from 'nodemailer';
+
+export const transporter = createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false 
+  },
+  logger: true,
+  debug: true
+});
+
+export default transporter;
